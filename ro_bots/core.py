399d5fe51cbe59cd368edc05d13b08bcc805516c
Mihai <mcosma@gmail.com>
2023-08-18 13:30:14 -0400

    pass trade_list to run_bots

--- ro_bots/core.py
+++ ro_bots/core.py
@@ -2,9 +2,9 @@
 from pkg_resources import resource_filename
 from ro_bots import run_hyperdrive_agents
 
-def run_bots():
+def run_bots(trade_list=None):
     """Run ro-bots."""
-    run_hyperdrive_agents.main(["--develop"], abi_folder=f"{resource_filename('ro_bots', 'abis')}")
+    run_hyperdrive_agents.main(["--develop"], trade_list=trade_list, abi_folder=f"{resource_filename('ro_bots', 'abis')}")
 
 if __name__ == "__main__":
     run_bots()
